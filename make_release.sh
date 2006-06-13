0f8f35e67c5df7ce6fb5ba3fb6bfe37543ddad23
mblondel <mblondel@59acd704-e115-0410-a914-e735a229ed7c>
2006-06-13 21:46:00 +0000

    added

---
+++ make_release.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+RM="`which rm` -vrf"
+MKDIR="`which mkdir` -p"
+
+if [ "$1foo" = "foo" ]; then
+        echo "usage: `basename $0` X.Y.Z"
+        exit 1
+fi
+
+PKG="wikipediafs-$1"
+TAR_PKG="$PKG.tar.gz"
+ZIP_PKG="$PKG.zip"
+TMP_DIR="/tmp/$PKG"
+
+echo "Uploading site..."
+scp site/*.htm \
+mblondel@shell.sourceforge.net:/home/groups/w/wi/wikipediafs/htdocs/
+
+echo "Creating temporary directory..."
+$RM $TMP_DIR
+$MKDIR $TMP_DIR
+cp -r * $TMP_DIR
+cd $TMP_DIR
+
+echo "Removing unnecessary files..."
+$RM `find . -name "*.pyc" -or -name ".*" -or -name "*~" -or -name "*.orig"`
+
+echo "Updating version number..."
+echo $1 > VERSION
+
+cd ..
+
+echo "Generating tarball..."
+$RM $TAR_PKG 
+tar -czf $TAR_PKG $PKG
+
+echo "Generating zip..."
+$RM $ZIP_PKG 
+zip -r $ZIP_PKG $PKG
+
+echo "Generated archives:"
+du -h "`dirname $TMP_DIR`/$TAR_PKG"
+du -h "`dirname $TMP_DIR`/$ZIP_PKG"
+
+exit 0
