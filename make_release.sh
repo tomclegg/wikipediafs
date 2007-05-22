f652822d93f7a5f39c2d98f4a6392af078c2d362
mblondel <mblondel@59acd704-e115-0410-a914-e735a229ed7c>
2007-05-22 20:10:54 +0000

    updated make release process : remove site/ and upload screenshots

--- make_release.sh
+++ make_release.sh
@@ -27,13 +27,14 @@ gzip doc/mount.wikipediafs.1
 
 echo "Uploading site..."
 cp README README.txt
-scp site/*.htm doc/mount.wikipediafs.htm README.txt \
+scp site/*.htm site/*.png doc/mount.wikipediafs.htm README.txt \
 mblondel@shell.sourceforge.net:/home/groups/w/wi/wikipediafs/htdocs/
 $RM README.txt
 
 echo "Removing unnecessary files..."
 $RM `find . -name "*.pyc" -or -name ".*" -or -name "*~" -or -name "*.orig"`
 $RM "make_release.sh"
+$RM site/
 
 echo "Updating version number..."
 echo "VERSION = '"$1"'" > src/wikipediafs/version.py
