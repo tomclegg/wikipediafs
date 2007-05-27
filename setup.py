9ab6aba1fd9dea9b24dbb9af7225a4fca6a430b9
mblondel <mblondel@59acd704-e115-0410-a914-e735a229ed7c>
2007-05-27 16:41:22 +0000

    setup.py: make the man page optional in the install process.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from distutils.core import setup
+import os
 
 # Please run
 # python setup.py install
 
+if os.path.exists('doc/mount.wikipediafs.1.gz'):
+    df = [('/usr/share/man/man1/', ['doc/mount.wikipediafs.1.gz'])]
+else:
+    df = []    
+
 setup(
     name = 'wikipediafs',
-    version = '0.1',
     author = 'Mathieu Blondel',
     author_email = 'mblondel@users.sourceforge.net',
     url = 'http://wikipediafs.sourceforge.net',
     packages = ['wikipediafs'],
     package_dir = {'wikipediafs':'src/wikipediafs/'},
     scripts = ['src/mount.wikipediafs'],
-    data_files = [('/usr/share/man/man1/', ['doc/mount.wikipediafs.1.gz'])],
+    data_files = df,
 )
